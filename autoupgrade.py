65d99a0c352cc7f1032db1f5d2a0469145e27b3f
Bukehi <l15510265616@163.com>
2024-05-17 18:29:22 +0800

    "upgrade commit"

--- autoupgrade.py
+++ autoupgrade.py
@@ -1,4 +1,4 @@
-from ex import resou, one, rmrb, weather
+from ex import resou, rmrb, weather, baidu, douyin, quanwang
 from datetime import datetime
 import time
 import configparser
@@ -13,9 +13,11 @@ date = str(datetime.now())[:10]
 if not upgradedate == date:
     rmrb.rmrb()
 # 创建内容
-one = one.one()
 content_1 = resou.baidu()
 content_2 = weather.getlocalweather()
+# content_3 = baidu.baidu()
+content_4 = douyin.douyin()
+content_5 = quanwang.quanwang()
 now = str(datetime.now())
 now = now[0:19]
 gaokao = datetime(2024, 6, 7)
@@ -24,8 +26,8 @@ dates = (gaokao-datetime.now()).days
 # 写入文件
 head = "本内容的收集整理及封装技术由Bukehi提供\n"
 with open('./livefile/热搜.txt', 'w', encoding='utf-8') as f:
-    f.write(head + f"{one}\n距高考还有{dates}天\n该内容更新于 {now}\n" + f"{content_2}\n"
-            "百度热搜\n" + content_1)
+    f.write(head + f"距高考还有{dates}天\n该内容更新于 {now}\n" + f"{content_2}\n"
+            "百度热搜\n" + content_1 + "抖音热搜\n" + content_4 + "全网热搜\n" + content_5)
 if upgradedate == date:
     subprocess.call(['git', 'add', 'livefile/热搜.txt'])
     subprocess.call(['git', 'add', '人民日报/人民日报.pdf'])
--- test.py
+++
@@ -1,3 +0,0 @@
-from ex import rmrb
-
-rmrb.rmrb()
